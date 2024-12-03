587330ba25613ac72d17d9d267c6f1c49d57aa84
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-12-03 15:29:12 +0100

    Init

---
+++ src/main/java/com/example/calculator/rest/controller/CalculatorController.java
@@ -0,0 +1,42 @@
+package com.example.calculator.rest.controller;
+
+import com.example.calculator.rest.service.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //mark this class as a REST controller, where methods return data (like JSON) instead of views.
+@RequestMapping("api/calculator") //define the base path of all operations
+
+public class CalculatorController {
+
+    @Autowired // injection of CalculatorService into the controller, allows the use of its methods for mathematical operation
+    private CalculatorService calculatorService;
+
+    // Handle GET request for sum
+    @GetMapping("/sum")   //Associate a URL and Http Get request with the logic methods
+    public int sum(@RequestParam int a, @RequestParam int b){   //capture query parameters from URL and pass them as methods parameters
+        // Call method sum of calculatorService and return the result
+        return calculatorService.sum(a, b);
+    }
+
+    // Handle GET request for subtraction
+    @GetMapping("/sub")
+    public int sub(@RequestParam int a, @RequestParam int b){
+        return calculatorService.subtraction(a, b);
+    }
+
+    // Handle GET request for multiplication
+    @GetMapping("/multiply")
+    public int multiply(@RequestParam int a, @RequestParam int b){
+        return calculatorService.multiplication(a, b);
+    }
+
+    // Handle GET request for division
+    @GetMapping("/div")
+    public int div(@RequestParam int a, @RequestParam int b){
+        return calculatorService.division(a, b);
+    }
+}
---
+++ src/main/java/com/example/calculator/rest/service/CalculatorService.java
@@ -0,0 +1,65 @@
+package com.example.calculator.rest.service;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CalculatorService {
+
+    /**
+     * Add the two integers.
+     *
+     * @param a first integer (numerator)
+     * @param b second integer (denominator)
+     * @return the sum of a and b
+     */
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Subtract the second integer from the first one.
+     *
+     * @param a first integer (numerator)
+     * @param b second integer (denominator)
+     * @return the resul of a minus b
+     */
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    /**
+     * Multiply the two integers.
+     *
+     * @param a first integer (numerator)
+     * @param b second integer (denominator)
+     * @return the product of a and b
+     */
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    /**
+     * Divide first integer by the second one.
+     *
+     * If the second integer (denominator) is zero, an ArithmeticException is caught,
+     * an error message is printed and zero is returned as result.
+     *
+     * @param a first integer (numerator)
+     * @param b second integer (denominator)
+     * @return the resul of a by b, or 0 if division is by 0
+     */
+    public int division(int a, int b) {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+
+            // handle division when denominator is 0: print an error message and 0 as result
+            System.out.println("Error: Division by zero");
+            return 0 ;
+
+        }
+
+    }
+
+}
