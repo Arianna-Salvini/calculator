a21eb7685aff792f34b8e512ed13383bdc4261d3
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-12-04 19:45:37 +0100

    Edit comments e and nesting

--- calculator_frontend/frontend/src/App.vue
+++ calculator_frontend/frontend/src/App.vue
@@ -1,53 +1,61 @@
 <template>
   <div class="calculator">
-    <!-- Display numeratore e denominatore -->
-    <div class="display">
-      <p>{{ numerator || "Enter your operation" }} {{ operation || "" }} {{ denominator || "" }}</p>
-      <p>{{ result || "" }}</p>
-    </div>
+    <!-- Display for numerator and denominator and result -->
+    <p>{{ numerator || "Enter your operation" }} {{ operator || "" }} {{ denominator || "" }}</p>
+    <p>{{ result || "" }}</p>
+  </div>
 
-    <!-- Operazioni sopra i numeri -->
-    <div class="operations">
-      <button @click="setOperation('+')">+</button>
-      <button @click="setOperation('-')">-</button>
-      <button @click="setOperation('*')">*</button>
-      <button @click="setOperation('/')">/</button>
-    </div>
+  <!-- If you click on a numeric operator move from numeratore to denominator -->
 
-    <!-- Tastiera numerica e pulsante uguale -->
-    <div class="main">
-      <!-- Tastiera numerica -->
-      <div class="buttons">
-        <div class="row">
-          <button @click="appendNumber(1)">1</button>
-          <button @click="appendNumber(2)">2</button>
-          <button @click="appendNumber(3)">3</button>
-        </div>
-        <div class="row">
-          <button @click="appendNumber(4)">4</button>
-          <button @click="appendNumber(5)">5</button>
-          <button @click="appendNumber(6)">6</button>
-        </div>
-        <div class="row">
-          <button @click="appendNumber(7)">7</button>
-          <button @click="appendNumber(8)">8</button>
-          <button @click="appendNumber(9)">9</button>
-        </div>
-        <div class="row">
-          <button @click="appendNumber(0)">0</button>
+  <div class="operators">
+    <button @="setOperation('+')">+</button>
+    <button @="setOperation('-')">-</button>
+    <button @="setOperation('*')">*</button>
+    <button @="setOperation('/')">/</button>
+  </div>
 
-          <!-- Clear button -->
-          <button class="clear" @click="clearInput">C</button>
-        </div>
+  <!-- Numeric keybord and equal button -->
+  <div class="numeric_keybord">
+    <!-- Numeric keybord -->
+    <div class="button">
+
+      <!-- Numbers 1 to 3 -->
+      <div class="row">
+        <button @click="appendNumber(1)">1</button>
+        <button @click="appendNumber(2)">2</button>
+        <button @click="appendNumber(3)">3</button>
       </div>
 
-      <!-- Pulsante uguale -->
+      <!-- Numbers 4 to 6 -->
+      <div class="row">
+        <button @click="appendNumber(4)">4</button>
+        <button @click="appendNumber(5)">5</button>
+        <button @click="appendNumber(6)">6</button>
+      </div>ù
+      <!-- Numbers 7 to 9 -->
+      <div class="row">
+        <button @click="appendNumber(7)">7</button>
+        <button @click="appendNumber(8)">8</button>
+        <button @click="appendNumber(9)">9</button>
+      </div>
+
+      <!-- Numbers 0 and clear button -->
+      <div class="row">
+        <button @click="appendNumber(0)">0</button>
+
+        <!-- C clear button -->
+        <button class="clear" @click="clearInput">C</button>
+      </div>
+
+      <!-- Equal button-->
       <div class="equal">
         <button @click="calculateResult">=</button>
       </div>
 
     </div>
+
   </div>
+
 </template>
 
 <script>
@@ -55,24 +63,25 @@
 </script>
 
 <style scoped>
+/* Calculator */
 .calculator {
   background-color: orange;
+  border-radius: 15px;
+  padding: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+
   display: flex;
   flex-direction: column;
   align-items: center;
-  border-radius: 15px;
-
-  padding: 20px;
-  font-family: Arial, sans-serif;
 }
 
 .display {
-  min-height: 60px;
   width: 100%;
+  min-height: 60px;
   background-color: #ddd;
   color: #000;
-  border-radius: 15px;
   text-align: center;
+  border-radius: 15px;
   margin-bottom: 20px;
 
   p {
@@ -80,80 +89,80 @@
   }
 }
 
+/* buttons */
 
-
-.operations {
+.operators {
   display: flex;
   justify-content: space-between;
   margin-bottom: 20px;
 }
 
-.operations button {
+buttons .operators {
   width: 55px;
   height: 55px;
-  /* Tondo per le operazioni */
-  border-radius: 50%;
   font-size: 25px;
-  padding: 5px;
-  margin: 0 5px;
-  cursor: pointer;
   border: 1px solid #8e8e8e;
-}
-
-.main {
-  display: flex;
-}
-
-.buttons {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-button {
-  font-size: 20px;
-  padding: 15px 20px;
+  border-radius: 50%;
+  padding: 5px;
   margin: 5px;
   cursor: pointer;
-  border-radius: 50%;
-  /* Pulsanti numerici tondi */
-  border: 1px solid #8e8e8e;
 }
 
-button:hover {
-  background-color: #cccccc;
-}
+/* numeric_keybord */
 
-/* Pulsante Clear con bordi arrotondati, non tondo */
-.clear {
-  width: 120px;
-  border-radius: 30px;
-  /* Solo arrotondato */
-}
-
-/* Pulsante Uguale con bordi arrotondati, non tondo */
-.equal {
+.numeric_keybord {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
 
-.equal button {
-  width: 60px;
-  height: 245px;
-  font-size: 30px;
-  border-radius: 30px;
-  /* Solo arrotondato */
-}
+  .buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-p {
-  font-size: 18px;
-  font-weight: bold;
+    .row {
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  button {
+    font-size: 20px;
+    padding: 15px 20px;
+    margin: 5px;
+    border: 1px solid #8e8e8e;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #cccccc;
+  }
+
+  /* ClearButton */
+
+  .clear {
+    width: 120px;
+    border-radius: 30px;
+  }
+
+  /* Equals Button */
+  .equal {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    >button {
+      width: 60px;
+      height: 245px;
+      font-size: 30px;
+      border-radius: 30px;
+
+      p {
+        font-size: 18px;
+        font-weight: bold;
+      }
+    }
+  }
+
 }
 </style>
