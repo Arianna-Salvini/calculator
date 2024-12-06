f08a7acbcc14cac329f64c755e6191c717483143
Arianna-Salvini <135632706+Arianna-Salvini@users.noreply.github.com>
2024-12-06 09:18:49 +0100

    Update CalculatorController

--- calculator_backend/src/main/java/com/example/calculator/rest/controller/CalculatorController.java
+++ calculator_backend/src/main/java/com/example/calculator/rest/controller/CalculatorController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController //mark this class as a REST controller, where methods return data (like JSON) instead of views.
 @RequestMapping("api/calculator") //define the base path of all operations
